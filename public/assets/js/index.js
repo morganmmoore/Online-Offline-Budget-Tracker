e0df07105709604b3dc925b3a893aff7e8499540
morganmmoore <morganmmoore0@gmail.com>
2021-12-08 20:40:26 -0500

    corrected mistake in service-worker.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,12 @@
 let transactions = [];
 let myChart;
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("Service Worker registered successfully."))
+    .catch(error => console.log("Service Worker registration failed:", error));
+};
+
 fetch("/api/transaction")
   .then(response => {
     return response.json();
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,46 +1,47 @@
 const FILES_TO_CACHE = [
     "/",
-    "/index",
+    "/index.html",
+    "/assets/js/db.js",
     "/assets/css/styles.css",
     "/assets/icons/icon-192x192.png",
     "/assets/icons/icon-512x512.png",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches
-      .open(STATIC_CACHE)
+      caches
+      .open(CACHE_NAME)
       .then(cache => cache.addAll(FILES_TO_CACHE))
       .then(() => self.skipWaiting())
   );
 });
-  
+
 self.addEventListener("activate", event => {
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
   event.waitUntil(
     caches
-      .keys()
-      .then(cacheNames => {
+    .keys()
+    .then(cacheNames => {
         return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
+            cacheName => !currentCaches.includes(cacheName)
         );
-      })
-      .then(cachesToDelete => {
+    })
+    .then(cachesToDelete => {
         return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
-          })
+            cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            })
         );
-      })
-      .then(() => self.clients.claim())
+    })
+    .then(() => self.clients.claim())
   );
 });
-  
+
 self.addEventListener("fetch", event => {
   if (
     event.request.method !== "GET" ||
@@ -49,10 +50,9 @@ self.addEventListener("fetch", event => {
     event.respondWith(fetch(event.request));
     return;
   }
-
-  if (event.request.url.includes("/api/images")) {
+  if (event.request.url.includes("/api/")) {
     event.respondWith(
-      caches.open(RUNTIME_CACHE).then(cache => {
+      caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(event.request)
           .then(response => {
             cache.put(event.request, response.clone());
@@ -63,16 +63,14 @@ self.addEventListener("fetch", event => {
     );
     return;
   }
-
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
       if (cachedResponse) {
         return cachedResponse;
       }
-
-      return caches.open(RUNTIME_CACHE).then(cache => {
+      return caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(event.request).then(response => {
-          return cache.put(event.request, response.clone()).then(() => {
+          return cache.put(event.request, response.clone()).then(() =>{
             return response;
           });
         });
@@ -80,4 +78,3 @@ self.addEventListener("fetch", event => {
     })
   );
 });
-  
