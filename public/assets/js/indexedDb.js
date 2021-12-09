cfa20b54c55a3a4009d01d81841769c5da5059d2
morganmmoore <morganmmoore0@gmail.com>
2021-12-08 20:07:46 -0500

    updated indexedDb.js and renamed db.js

--- public/assets/js/indexedDb.js
+++ public/assets/js/indexedDb.js
@@ -20,9 +20,34 @@ request.onsuccess = (event) => {
 };
 
 function saveRecord(record) {
-  const transaction = db.transaction("pending", "readwrite");
+  const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
 
   store.add(record);
 };
 
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+
+        store.clear();
+      });
+    }
+  }
+};
